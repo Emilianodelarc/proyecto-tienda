3ae564800c4f723d2ee58644f8257122ef943119
Emiliano Del Arco <emilianodelarc@gmail.com>
2021-02-04 17:28:37 -0300

    agragado de factura

--- js/script.js
+++ js/script.js
@@ -50,13 +50,8 @@ var baseDatos = [];
 function agregar(){
     baseDatos.push(nuevoProducto);
     //console.log(baseDatos);
-    document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+nuevoProducto.id+'"><img src='+imagenCapturar+' class="img-fluid"/><span class="item-name">'+tituloCapturar+'</span><span class="item-price">'+precioCaptu+'</span><button class="btn boton-cerrar" onclick="borrar('+ nuevoProducto.id +');">X</button></li>';
-    
-    /*carritoJSON=JSON.stringify(baseDatos);
-    nuevoUser = localStorage.getItem('nombre');
-    
-    localStorage.setItem('carrito_' + nuevoUser, carritoJSON);
-    carrito1= localStorage.getItem('carrito_'+nuevoUser)*/
+   document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+nuevoProducto.id+'"><img src='+imagenCapturar+' class="img-fluid"/><span class="item-name">'+tituloCapturar+'</span><span class="item-price">'+precioCaptu+'</span><button class="btn boton-cerrar" onclick="borrar('+ nuevoProducto.id +');">X</button></li>';
+
     carritoLS();
     sumar();
     
@@ -93,17 +88,15 @@ function borrar(producto){
     
     listaFunc.removeChild(borrarElemento);
 
-    productoDel= document.querySelector('[data-id="'+producto+'"]')
-    
     for (var i =0; i < baseDatos.length; i++){
-        if (baseDatos[i].id === nuevoProducto.id) {
+        if (baseDatos[i].id == producto) {
            baseDatos.splice(i,1);
-           console.log(nuevoProducto.id)
+           
         }
      };
     
     //baseDatos.shift();
-
+     carritoLS();
      sumar();
 
     console.log(baseDatos)
@@ -117,15 +110,7 @@ function carritoLS(){
     
      
     carritoJSON=JSON.stringify(baseDatos);
-    nuevoUser = localStorage.getItem('nombre');
-    
-    localStorage.setItem('carrito_' + nuevoUser, carritoJSON);
-    carrito1= localStorage.getItem('carrito_'+ nuevoUser)
-    obj = JSON.parse(carrito1);
-    //console.log(obj);
-    
-    //document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+obj.id+'"><img src='+obj.imagen+' class="img-fluid"/><span class="item-name">'+obj.titulo+'</span><span class="item-price">'+Obj.precio+'</span><button class="btn boton-cerrar" onclick="borrar('+ obj.id +');">X</button></li>';
-    //console.log(carrito1);
+    localStorage.baseDatos = carritoJSON;
 }
 
 
@@ -135,9 +120,12 @@ botonr.addEventListener('click', capturarUsuario);
 function capturarUsuario(){
     
     nombre = document.getElementById('nombre').value;
+    email=document.getElementById('email').value;
+
     //console.log(nombre);
     
     localStorage.setItem("nombre", nombre);
+    localStorage.setItem('email', email);
     
     datosUser()
 }
@@ -146,11 +134,93 @@ function capturarUsuario(){
 function datosUser(){
     usuario = localStorage.getItem('nombre');
     //console.log(usuario);
-    document.getElementById("nombreUsuario1").innerHTML = '<li>BIENVENIDO '+usuario+ ' <button onclick="checout();" id="salir">X</button</li>'
-
-
-    function checout(){
-        localStorage.clear();
-        document.location.reload(true);
+    if(!usuario){
+        document.getElementById("nombreUsuario1").innerHTML = '<li>BIENVENIDO! veo que no estas Registrado!</li>'
     }
+    else {
+        document.getElementById("nombreUsuario1").innerHTML = '<li>BIENVENIDO '+usuario+ ' <button onclick="checout();" id="salir"><i class="fas fa-sign-out-alt"></i></button</li>'
+
+        if (localStorage.baseDatos){
+            console.log("Terminá tu compra! Este es tu carrito:")
+            console.log(JSON.parse(localStorage.baseDatos))}
+    }
+}
+function checout(){
+    localStorage.clear();
+    document.location.reload(true);
+}
+datosUser()
+
+
+function leerLocalStorage () {
+
+    dataBaseLS = JSON.parse(localStorage.baseDatos);
+   
+    dataBaseLS.forEach(function (data) {
+
+      data = document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+data.id+'"><img src='+data.imagen+' class="img-fluid"/><span class="item-name">'+data.titulo+'</span><span class="item-price">'+data.precio+'</span><button class="btn boton-cerrar" onclick="borrarLS('+ data.id +');">X</button></li>';
+    })
+    sumarLS();
+}
+leerLocalStorage();
+
+function borrarLS(producto){
+    var listaFunc = document.getElementById("listaPro");
+    
+    //var borrarElemento = document.querySelector("li.clearfix[data-id=' " + producto + "'] ");
+    borrarElemento = document.querySelector("li.clearfix[data-id='"+producto+"']")
+    
+    listaFunc.removeChild(borrarElemento);
+
+    for (var i =0; i < dataBaseLS.length; i++){
+        if (dataBaseLS[i].id == producto) {
+           dataBaseLS.splice(i,1);
+           
+        }
+     };
+     
+     carritoLS();
+    sumarLS();
+    //baseDatos.shift();
+    console.log(dataBaseLS)
+    
+}
+function sumarLS(){
+    conteoCant = dataBaseLS.length;
+    document.getElementById("cont1").innerHTML = conteoCant;
+    document.getElementById("cont2").innerHTML = conteoCant
+
+    valor2 = document.getElementById('total').textContent;
+    
+    
+    // Aquí valido si hay un valor previo, si no hay datos, le pongo un cero "0".
+    valor2 = (valor2 == null || valor2 == undefined || valor2 == "") ? 0 : valor2;
+    
+    /* Esta es la suma. */
+    valor2 = dataBaseLS.reduce((acc, el) => acc + el.precio,0);
+    
+    
+    // Colocar el resultado de la suma en el control "span".
+    document.getElementById('total').innerHTML ='$' + valor2;
+}
+function compra(){
+    user= localStorage.getItem('nombre');
+    email=localStorage.getItem('email');
+    console.log(user,email)
+
+    cliente=document.getElementById('cliente').value = user;
+    correo=document.getElementById('correo').value = email;
+
+    
+};
+compra();
+registro();
+function registro(){
+    
+    dataBaseLS.forEach(function (item) {
+        item = document.getElementById('listaProd').innerHTML += '<td><img src="'+item.imagen+'" class="img-fluid"></td><td>'+item.titulo+'</td><td>'+item.precio+'</td>'
+    });
+   total = document.getElementById('totalFact').innerHTML = valor2;
+    subtotal= document.getElementById('subtotalFact').innerHTML = valor2 / 1.21;
+    document.getElementById('iva').innerHTML = (total - subtotal);
 }
