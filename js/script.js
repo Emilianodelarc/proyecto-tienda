02f278d72eee59285aab62fec085fdbc95926d7b
Emiliano Del Arco <emilianodelarc@gmail.com>
2021-02-05 18:18:20 -0300

    retoques en el codigo js sobre factura

--- js/script.js
+++ js/script.js
@@ -14,10 +14,11 @@ const boton = document.querySelectorAll('button#agregar-carrito')
     
     item.addEventListener("click", capturar);
 });
+
 //------------CAPTURO LOS PRODUCTOS -----------------------------------------------------------------------------------------------------
+
 function capturar(event){
-    //console.log("capturado")
-    //console.log(event.target.parentElement.parentElement.children);
+    
     var divProducto = event.target.parentElement.parentElement.children;
     var idProducto = event.target.parentElement.parentElement.getAttribute('data-id');
     
@@ -26,33 +27,38 @@ function capturar(event){
         this.titulo = titulo;
         this.precio = precio;
         this.id = id;
-    }
+    };
+
     imagenCapturar = divProducto[0].src;
-    //console.log(imagenCapturar);
+   
     tituloCapturar = divProducto[1].textContent;
-    //console.log(tituloCapturar);
+    
     precioCapturar = divProducto[4].textContent;
     precioCapturado = precioCapturar.replace(/[$.]/g,'');
     precioCaptu = parseInt(precioCapturado);
     
     nuevoProducto = new Producto (imagenCapturar, tituloCapturar,precioCaptu, idProducto);
-    //console.log(nuevoProducto);
+    
     agregar();
-}
-//------------------ACA SE AGREGAN LOS PRODUCTOS Y SE COLOCAN EN UN ARRAY Y SE LOS LLEVA AL LOCASTORAGE-----------------------------------------------------------
+};
+
+//------------------ACA SE AGREGAN LOS PRODUCTOS Y SE COLOCAN EN UN ARRAY-----------------------------------------------------------
 var baseDatos = [];
 
 function agregar(){
     baseDatos.push(nuevoProducto);
-    //console.log(baseDatos);
+    
    document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+nuevoProducto.id+'"><img src='+imagenCapturar+' class="img-fluid"/><span class="item-name">'+tituloCapturar+'</span><span class="item-price">'+precioCaptu+'</span><button class="btn boton-cerrar" onclick="borrar('+ nuevoProducto.id +');">X</button></li>';
 
+    factura();
     carritoLS();
     sumar();
     
-}
+};
+
 //---------------------------------------------------------------------------------------------------
 //------------ SUMA LOS PRECIO DE LOS PRODUCTOS ALAMACENADOS EN EL ARRAY-----------------------------
+
 function sumar () {
     
     conteoCant = baseDatos.length;
@@ -61,24 +67,24 @@ function sumar () {
     
     valor = document.getElementById('total').textContent;
     
-    
     // Aquí valido si hay un valor previo, si no hay datos, le pongo un cero "0".
     valor = (valor == null || valor == undefined || valor == "") ? 0 : valor;
     
     /* Esta es la suma. */
     valor = baseDatos.reduce((acc, el) => acc + el.precio,0);
     
-    
-    // Colocar el resultado de la suma en el control "span".
     document.getElementById('total').innerHTML ='$' + valor;
-    //console.log(valor);
-}
+    total = document.getElementById('totalFact').innerHTML = valor;
+    subtotal= document.getElementById('subtotalFact').innerHTML = parseInt(valor / 1.21);
+    document.getElementById('iva').innerHTML = (total - subtotal);
+   
+};
+
 //--------------------------------------------------------------------------------------------------------------
-//-------------ESTA FUNCION BORRA EL PRODUCTO QUE EL USUARIO NO QUIERE-------------------------------------------------------
+//-------------ESTA FUNCION BORRA EL PRODUCTO QUE EL USUARIO NO QUIERE DEL CARRITO Y POR ENDE SE BORRA DEL CARRITO------------------------------------------------------
 function borrar(producto){
     var listaFunc = document.getElementById("listaPro");
-    
-    //var borrarElemento = document.querySelector("li.clearfix[data-id=' " + producto + "'] ");
+
     borrarElemento = document.querySelector("li.clearfix[data-id='"+producto+"']")
     
     listaFunc.removeChild(borrarElemento);
@@ -89,38 +95,71 @@ function borrar(producto){
             
         }
     };
-    
-    //baseDatos.shift();
+
+    var listaFact = document.getElementById("listaProd");
+    var borrarTr=document.querySelector('tr.pro[data-id="'+producto+'"]')
+   
+    listaFact.removeChild(borrarTr);
+
+    for (var i =0; i < baseDatos.length; i++){
+        if (baseDatos[i].id == producto) {
+            baseDatos.splice(i,1);
+            
+        }
+    };
+   
     carritoLS();
     sumar();
-    
-    //console.log(baseDatos)  
 }
+//------------------------------FACTURA DE COMPRA-----------------------
+function factura(){
+    fact=document.getElementById('listaProd');
+    fact.innerHTML="";
+    baseDatos.forEach(function (prod){
+        prod = document.getElementById('listaProd').innerHTML += 
+        `<tr class="pro" data-id="${prod.id}">
+            <td>${prod.titulo}</td>
+            <td>${prod.precio}</td>
+        </tr>`;
+    });
+   
+};
+
 //----------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------DESDE AQUI TODO FUNCIONA CON LOCAL STORAGE--------------------------------------------------------------
 //----------------------CARRITO ENVIADO A LOCALSTORAGE------------------------------------------------------------------
+
 function carritoLS(){
     
     carritoJSON=JSON.stringify(baseDatos);
     localStorage.baseDatos = carritoJSON;
-}
+    
+};
+
 //--------------------------------------------------------------------------------------------------------------------------
 //-------------------------------CAPTURA LOS DATOS DEL USUARIO---------------------------------------------------------------
-var botonr = document.querySelector('button#registro')
+
+var botonr = document.querySelector('button#registro');
 botonr.addEventListener('click', capturarUsuario);
+
 function capturarUsuario(){
     
     nombre = document.getElementById('nombre').value;
-    email=document.getElementById('email').value;
-    
+    emailCli=document.getElementById('email').value;
+
+    nombreCli=document.getElementById('cliente').value = nombre;
+    correoCli=document.getElementById('correo').value = emailCli;
     //console.log(nombre);
     
     localStorage.setItem("nombre", nombre);
-    localStorage.setItem('email', email);
+    localStorage.setItem('email', emailCli);
     
-    datosUser()
-}
-//--------------------------------------------------------------------------------------
-//------------------DATOS DEL USUARIO---------------------------------------------------
+    datosUser();
+};
+
+//----------------------------------------------------------------------------------------------------------------------------------
+//------------------DATOS DEL USUARIO------------------------------------------------------------------------------------------------
+
 function datosUser(){
     usuario = localStorage.getItem('nombre');
     //console.log(usuario);
@@ -132,90 +171,125 @@ function datosUser(){
         
         if (localStorage.baseDatos){
             console.log("Terminá tu compra! Este es tu carrito:")
-            console.log(JSON.parse(localStorage.baseDatos))}
-        }
-    }
-    function checout(){
-        localStorage.clear();
-        document.location.reload(true);
-    }
-    datosUser()
+            console.log(JSON.parse(localStorage.baseDatos))
+        };
+    };
+};
+
+function checout(){
+    localStorage.clear();
+    document.location.reload(true);
+};
+
+    datosUser();
+
 //--------------------------------------------------------------------------------------------------------------
 //----------------------LECTURA DE LOCLSTORAGE Y CARGA LOS PRODUCTOS ALMACENADOS--------------------------------   
-    function leerLocalStorage () {
+
+function leerLocalStorage () {
+    
+    dataBaseLS = JSON.parse(localStorage.baseDatos);
+    
+    dataBaseLS.forEach(function (data) {
         
-        dataBaseLS = JSON.parse(localStorage.baseDatos);
-        
-        dataBaseLS.forEach(function (data) {
-            
-            data = document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+data.id+'"><img src='+data.imagen+' class="img-fluid"/><span class="item-name">'+data.titulo+'</span><span class="item-price">'+data.precio+'</span><button class="btn boton-cerrar" onclick="borrarLS('+ data.id +');">X</button></li>';
-        })
-        sumarLS();
-    }
+        data = document.getElementById("listaPro").innerHTML += '<li class="clearfix" data-id="'+data.id+'"><img src='+data.imagen+' class="img-fluid"/><span class="item-name">'+data.titulo+'</span><span class="item-price">'+data.precio+'</span><button class="btn boton-cerrar" onclick="borrarLS('+ data.id +');">X</button></li>';
+    });
+    sumarLS();
+};
+
 leerLocalStorage();
+
 //--------------------------------------------------------------------------------------------------------------    
 //---------------------BORRA LOS PRODUCTOS ALMACENDOS EN EL LOCAL STORAGE---------------------------------------  
-    function borrarLS(producto){
-        var listaFunc = document.getElementById("listaPro");
-        
-        //var borrarElemento = document.querySelector("li.clearfix[data-id=' " + producto + "'] ");
-        borrarElemento = document.querySelector("li.clearfix[data-id='"+producto+"']")
-        
-        listaFunc.removeChild(borrarElemento);
-        
-        for (var i =0; i < dataBaseLS.length; i++){
-            if (dataBaseLS[i].id == producto) {
-                dataBaseLS.splice(i,1);
-                
-            }
+
+function borrarLS(producto){
+    var listaFunc = document.getElementById("listaPro");
+    
+    //var borrarElemento = document.querySelector("li.clearfix[data-id=' " + producto + "'] ");
+    borrarElemento = document.querySelector("li.clearfix[data-id='"+producto+"']")
+    
+    listaFunc.removeChild(borrarElemento);
+    
+    for (var i =0; i < dataBaseLS.length; i++){
+        if (dataBaseLS[i].id == producto) {
+            dataBaseLS.splice(i,1);
+            
         };
-        
-        carritoLS();
-        sumarLS();
-        //baseDatos.shift();
-        console.log(dataBaseLS)
-        
-    }
+    };
+    
+    carritoLS();
+    sumarLS();
+    //baseDatos.shift();
+    console.log(dataBaseLS);
+    borrarFactu(data.id);
+};
+
 //----------------------------------------------------------------------------------------------------------------    
 //---------------------sUMA LOS PRODUCTOS DEL LOCAL STORAGE-------------------------------------------------------
-    function sumarLS(){
-        conteoCant = dataBaseLS.length;
-        document.getElementById("cont1").innerHTML = conteoCant;
-        document.getElementById("cont2").innerHTML = conteoCant
-        
-        valor2 = document.getElementById('total').textContent;
-        
-        
-        // Aquí valido si hay un valor previo, si no hay datos, le pongo un cero "0".
-        valor2 = (valor2 == null || valor2 == undefined || valor2 == "") ? 0 : valor2;
-        
-        /* Esta es la suma. */
-        valor2 = dataBaseLS.reduce((acc, el) => acc + el.precio,0);
-        
-        
-        // Colocar el resultado de la suma en el control "span".
-        document.getElementById('total').innerHTML ='$' + valor2;
-        registroLS();
-    }
+
+function sumarLS(){
+    conteoCant = dataBaseLS.length;
+    document.getElementById("cont1").innerHTML = conteoCant;
+    document.getElementById("cont2").innerHTML = conteoCant
+    
+    valor2 = document.getElementById('total').textContent;
+    
+    
+    // Aquí valido si hay un valor previo, si no hay datos, le pongo un cero "0".
+    valor2 = (valor2 == null || valor2 == undefined || valor2 == "") ? 0 : valor2;
+    
+    /* Esta es la suma. */
+    valor2 = dataBaseLS.reduce((acc, el) => acc + el.precio,0);
+    
+    
+    // Colocar el resultado de la suma en el control "span".
+    document.getElementById('total').innerHTML ='$' + valor2;
+    registroLS();
+};
+
 //----------------------------------------------------------------------------------------------------------------------
 //------------------------REALIZA LA CARGA DE DATOS PARA LA FACTURA-----------------------------------------------------
-    function compra(){
-        user= localStorage.getItem('nombre');
-        email=localStorage.getItem('email');
-        console.log(user,email)
-        
-        cliente=document.getElementById('cliente').value = user;
-        correo=document.getElementById('correo').value = email;              
-    };
-    compra();
+
+function compra(){
+    user= localStorage.getItem('nombre');
+    email=localStorage.getItem('email');
+    console.log(user,email)
     
-    function registroLS(){
-        
-        dataBaseLS.forEach(function (item) {
-            item = document.getElementById('listaProd').innerHTML += '<td><img src="'+item.imagen+'" class="img-fluid"></td><td>'+item.titulo+'</td><td>'+item.precio+'</td>'
-        });
-        total = document.getElementById('totalFact').innerHTML = valor2;
-        subtotal= document.getElementById('subtotalFact').innerHTML = parseInt(valor2 / 1.21);
-        document.getElementById('iva').innerHTML = (total - subtotal);
-    };
-//---------------------------------------------------------------------------------------------------------------------------
+    cliente=document.getElementById('cliente').value = user;
+    correo=document.getElementById('correo').value = email;              
+};
+
+compra();
+
+function registroLS(){
+    factLS=document.getElementById('listaProd');
+    factLS.innerHTML="";
+    dataBaseLS.forEach(function (item) {
+        item = document.getElementById('listaProd').innerHTML += 
+        `<tr class="pro" data-id="${item.id}">
+            <td>${item.titulo}</td>
+            <td>${item.precio}</td>
+        </tr>`;
+    });
+    total = document.getElementById('totalFact').innerHTML = valor2;
+    subtotal= document.getElementById('subtotalFact').innerHTML = parseInt(valor2 / 1.21);
+    document.getElementById('iva').innerHTML = (total - subtotal);
+    
+};
+
+//---------------------------BORRA LA LISTA DE LOS PRODUCTOS EN LA FACTURA-----------------------------------------------------
+
+function borrarFactu(producto){
+    var listaFact = document.getElementById("listaProd");
+   var borrarTr=document.querySelector('tr.pro[data-id="'+producto+'"]')
+   
+   listaFact.removeChild(borrarTr);
+
+    for (var i =0; i < baseDatos.length; i++){
+        if (baseDatos[i].id == producto) {
+            baseDatos.splice(i,1);
+            
+        };
+    }
+    
+};
